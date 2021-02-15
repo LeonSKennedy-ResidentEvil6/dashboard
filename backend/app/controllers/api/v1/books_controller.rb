bd7f7b9327e1dcc81917ab41eb2e88e69ac1bdb7
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-15 13:44:40 -0800

    added create action for book controller and actions for review controller

--- backend/app/controllers/api/v1/books_controller.rb
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::BooksController < ApplicationController
+  def index
+    books = Book.all
+    render json: books
+  end
 
-    def index
-        @books = Books.all
-        render json: @books
-    end 
+  def create
+    book = Book.new(books_params)
+    if book.save
+      render json: book, status: :accepted
+    else
+      render json: { errors: book.errors.full_messages }, statues: :unprocessible_entity
+    end
+  end
 
-    def books_params
-        params.permit(:title, :author, :description, :description)
-    end 
-    
+  private
+  
+  def books_params
+    params.require(:book).permit(:title, :author, :description, :description)
+  end
 end
--- backend/app/controllers/api/v1/reviews_controller.rb
+++ backend/app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::ReviewsController < ApplicationController
+
+
+    def index
+        reviews = Review.all
+        render json: reviews
+    end 
+
+    def create
+        review = Review.new(review_params)
+    end 
+
+    def review_params
+        params.require(:review).permit(:comment, :book_id)
+    end 
+
 end
