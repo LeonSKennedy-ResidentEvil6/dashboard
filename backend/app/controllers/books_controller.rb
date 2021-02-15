88b5842b3514bc13247e2b224ffe996ce3356b81
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-15 12:39:24 -0800

    changed mirgation belongs to reference for sys efficiency

--- backend/app/controllers/books_controller.rb
+++ backend/app/controllers/books_controller.rb
@@ -1,18 +1,6 @@
 class BooksController < ApplicationController
 
-    def show 
-    end 
-
     def index
-    end 
-    
-    def new
-    end 
-
-    def create
-    end 
-
-    def delete
     end 
 
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_000627) do
+ActiveRecord::Schema.define(version: 2021_02_15_181704) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -23,9 +23,11 @@ ActiveRecord::Schema.define(version: 2021_02_15_000627) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
-    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
+  add_foreign_key "reviews", "books"
 end
