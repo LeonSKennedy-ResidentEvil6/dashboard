20375c8eab7a412621379d0b036b8b3100231ff0
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-15 12:54:17 -0800

    generated relevant route

--- backend/app/controllers/api/v1/books_controller.rb
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::BooksController < ApplicationController
+
+    def index
+        @books = Books.all
+        render json: @books
+    end 
+
+    def books_params
+        params.permit(:title, :author, :description, :description)
+    end 
+    
 end
