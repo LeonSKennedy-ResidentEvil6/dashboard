031a428133851a3a765d61c21dc64851c5cebd65
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-19 17:20:14 -0800

    edited

--- frontend/src/review.js
+++ frontend/src/review.js
@@ -13,5 +13,48 @@ class Review {
             <br>`;
     }
 
-    
+    static createNewReview() {
+        const comment = document.getElementById("input-reviews").value
+        const bookId = currentBookId
+
+        let review = {
+            comment: comment,
+            book_id: bookId
+        }
+
+        let reviewObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(review)
+        }
+
+        fetch(BOOKS_URL + book_id + "/reviews", reviewObj)
+            .then(response => response.json())
+            .then(review => {
+                let newReview = new Review(review)
+                newReview.renderReview();
+            })
+    }
+
+    static fetchReviews(id) {
+        document.getElementById("reviewsList").innerHTML = '';
+        fetch(BOOKS_URL + id + "/reviews")
+            .then(response => response.json())
+            .then(reviews => {
+                for (const review of reviews) {
+                    let review = new Review(review);
+                    review.renderReview();
+                }
+            })
+    }
+
+    static toggleReviews(id) {
+        (e) => { e.preventDefault() };
+        currentBookId = id;
+        this.fetchReviews(id);
+    }
+
 }
