c926f41b83db9eef6bc09eb34349c056f887c32f
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-20 11:36:06 -0800

    modified review constructor, refactor renderview, append review element, add delete button to remove review in review is

--- frontend/src/review.js
+++ frontend/src/review.js
@@ -2,59 +2,32 @@ class Review {
     constructor(review) {
         this.id = review.id,
         this.comment = review.comment,
-        this.book_id = review.book_id
+        this.book = review.book_id
+
+        Review.all.push(this)
     }
 
     renderReview() {
-        return `
-            <div review-id=${this.id}>
-                <h2>Comment: ${this.comment}</h2>
+        const reviewList = document.querySelector('#review-container')
+        const reviewMarkup =  `
+            <br>
+            <div class="review" id=${this.id} style="border: 4px red">
+                <h2>Review: ${this.comment}</h2>
             </div>
-            <br>`;
-    }
+            <br>
+        `;
 
-    static createNewReview() {
-        const comment = document.getElementById("input-reviews").value
-        const bookId = currentBookId
+        let reviewElement = document.createElement('div')
+        reviewElement.innerHTML = reviewMarkup
+        reviewList.appendChild(reviewElement)
 
-        let review = {
-            comment: comment,
-            book_id: bookId
-        }
-
-        let reviewObj = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(review)
-        }
-
-        fetch(BOOKS_URL + book_id + "/reviews", reviewObj)
-            .then(response => response.json())
-            .then(review => {
-                let newReview = new Review(review)
-                newReview.renderReview();
-            })
-    }
-
-    static fetchReviews(id) {
-        document.getElementById("reviewsList").innerHTML = '';
-        fetch(BOOKS_URL + id + "/reviews")
-            .then(response => response.json())
-            .then(reviews => {
-                for (const review of reviews) {
-                    let review = new Review(review);
-                    review.renderReview();
-                }
-            })
-    }
-
-    static toggleReviews(id) {
-        (e) => { e.preventDefault() };
-        currentBookId = id;
-        this.fetchReviews(id);
+        let deleteReview = document.createElement('button')
+        deleteReview.setAttribute("id", this.id)
+        deleteReview.innerHTML = "Delete this review"
+        deleteReview.addEventListener("click", (evnt) => deleteReview(evnt))
+        reviewList.appendChild(deleteReview)
     }
 
 }
+
+Review.all = [];
